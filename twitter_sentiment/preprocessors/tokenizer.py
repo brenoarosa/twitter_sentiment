48599d51ae4454cf681bcc68bd2e9204e249cad5
Breno Arosa <brenoarosa@hotmail.com>
2020-06-28 19:45:52 -0300

    Adds tokenizer option to remove punctuation

--- twitter_sentiment/preprocessors/tokenizer.py
+++ twitter_sentiment/preprocessors/tokenizer.py
@@ -1,18 +1,21 @@
+import string
 from typing import Iterable
 import nltk
 from nltk.corpus import stopwords
 
 
 def tokenize(tweets: Iterable[dict], lang: str = None, reduce_len: bool = True,
-             remove_single: bool = False, remove_stopwords: bool = False) -> Iterable[dict]:
+             remove_single: bool = False, remove_stopwords: bool = False,
+             remove_punctuation: bool = True) -> Iterable[dict]:
 
     if remove_stopwords and (not lang):
         raise RuntimeError("Missing language parameter")
 
     if lang:
         lang_map = {"pt": "portuguese", "en": "english"}
-        tokenizer = nltk.tokenize.casual.TweetTokenizer(reduce_len=reduce_len)
         stop = set(stopwords.words(lang_map[lang]))
+
+    tokenizer = nltk.tokenize.casual.TweetTokenizer(reduce_len=reduce_len)
 
     for tweet in tweets:
         text = tweet["treated_text"]
@@ -20,10 +23,13 @@ def tokenize(tweets: Iterable[dict], lang: str = None, reduce_len: bool = True,
 
         # removes tweets with single word
         if remove_single:
-            tokens = filter(lambda x: len(x) >= 2, tokens)
+            tokens = [t for t in tokens if len(t) >= 2]
 
         if remove_stopwords:
-            tokens = filter(lambda x: x not in stop, tokens)
+            tokens = [t for t in tokens if (t not in stop)]
+
+        if remove_punctuation:
+            tokens = [t for t in tokens if (t not in string.punctuation)]
 
         tweet["tokenized_treated_text"] = tokens
         # TODO maybe remove punctuation tokens?
