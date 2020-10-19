2cc5ce630928e728af91960e008bb02f7b7c069b
Breno Arosa <brenoarosa@hotmail.com>
2020-10-19 14:39:43 -0300

    Adds model training to elmo script

--- twitter_sentiment/text/elmo.py
+++ twitter_sentiment/text/elmo.py
@@ -2,15 +2,13 @@ import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.utils.class_weight import compute_class_weight
-from gensim.models import Word2Vec
 from tensorflow.keras import models, layers, losses, regularizers, callbacks
 from twitter_sentiment.preprocessors.utils import read_jsonlines_lzma
 from twitter_sentiment.text.embedding import load_w2v_weight_and_X
 from twitter_sentiment.preprocessors.distant_supervision import extract_tweets_tokenized_text_and_Y
 from allennlp.commands.elmo import ElmoEmbedder
 
-MAX_LEN = 50
-lens = []
+MAX_LEN = 60
 
 def pad_elmo_tokens(tokenized_texts: list) -> list:
     """
@@ -21,6 +19,7 @@ def pad_elmo_tokens(tokenized_texts: list) -> list:
         tokenized_texts[i].append("</S>")
 
     return tokenized_texts
+
 
 class TweetDataGenerator(tf.keras.utils.Sequence):
 
@@ -44,7 +43,7 @@ class TweetDataGenerator(tf.keras.utils.Sequence):
         indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
 
         batch_tokenized_texts = [self.tokenized_texts[i] for i in indexes]
-        batch_Y = [self.Y[i] for i in indexes]
+        batch_Y = np.array([self.Y[i] for i in indexes])
 
         # TODO: precisa forcar o tamanho pra um valor maximo pro shape ficar igual https://pytorch.org/docs/stable/nn.functional.html#torch.nn.functional.pad
         # cuidado que esse pad e 2d, outra opcao iniciar outro tensor zerado de tamanho fixo
@@ -54,16 +53,18 @@ class TweetDataGenerator(tf.keras.utils.Sequence):
         pt_tensor = pt_tensor.permute(0, 2, 1, 3).reshape(batch_size, seq_len, -1)
         batch_X = pt_tensor.numpy()
 
-        lens.append(seq_len)
+        seq_len = min(seq_len, MAX_LEN)
+
         fixed_size_batch_X = np.zeros((batch_size, MAX_LEN, elmo_outputs*emb_dim))
-        fixed_size_batch_X[0:batch_size, 0:MAX_LEN, 0:elmo_outputs*emb_dim] = batch_X[0:batch_size, 0:MAX_LEN, 0:elmo_outputs*emb_dim]
+        fixed_size_batch_X[0:batch_size, 0:seq_len, 0:elmo_outputs*emb_dim] = batch_X[0:batch_size, 0:seq_len, 0:elmo_outputs*emb_dim]
 
         return fixed_size_batch_X, batch_Y
 
     def on_epoch_end(self):
         self.indexes = np.arange(len(self.Y))
-        if self.shuffle == True:
+        if self.shuffle:
             np.random.shuffle(self.indexes)
+
 
 def train_model(filepath: str):
     tweets = read_jsonlines_lzma(filepath)
@@ -80,40 +81,18 @@ def train_model(filepath: str):
     train_data_gen = TweetDataGenerator(tokenized_texts_train, y_train, embedder, batch_size=32, shuffle=True)
     test_data_gen = TweetDataGenerator(tokenized_texts_test, y_test, embedder, batch_size=32, shuffle=True)
 
-    from datetime import datetime
-
-    import ipdb; ipdb.set_trace()
-
-    for i in range(100):
-        train_data_gen[i]
-        print(datetime.now())
-
-    return
-
-
-    pt_tensor, _ = embedder.batch_to_embeddings(tokenized_texts[0:512]) # FIXME: huge memory constrain, maybe a dataloader?
-    batch_size, elmo_outputs, seq_len, emb_dim = pt_tensor.shape
-    # concatenate all emb_dimensions
-    pt_tensor = pt_tensor.permute(0, 2, 1, 3).reshape(batch_size, seq_len, -1)
-    X = pt_tensor.numpy()
-    return
-
-
-    seq_len = 40 # FIXME
-    w2v = Word2Vec.load(embedding_path)
-    emb_weights, X = load_w2v_weight_and_X(w2v, tokenized_texts, seq_len=seq_len)
-
     model = models.Sequential()
-    model.add(layers.Embedding(emb_weights.shape[0], emb_weights.shape[1],
-                               input_length=seq_len,
-                               weights=[emb_weights],
-                               trainable=False))
+    model.add(layers.Input(shape=(MAX_LEN, 3072)))
 
-    model.add(layers.Bidirectional(layers.LSTM(64, return_sequences=True)))
-    model.add(layers.Bidirectional(layers.LSTM(64)))
-
+    model.add(layers.Conv1D(filters=200, kernel_size=3, padding="same",
+                            kernel_regularizer=regularizers.l2(10**-3),
+                            bias_regularizer=regularizers.l2(10**-3)))
+    model.add(layers.BatchNormalization())
+    model.add(layers.Activation("relu"))
+    model.add(layers.MaxPooling1D(3))
     model.add(layers.Dropout(.5))
 
+    model.add(layers.Flatten())
     model.add(layers.Dense(1,
                            activation="sigmoid",
                            kernel_regularizer=regularizers.l2(10**-3),
@@ -126,16 +105,17 @@ def train_model(filepath: str):
     classes = np.unique(Y).tolist()
     class_weights = dict(zip(classes, compute_class_weight("balanced", classes=classes, y=Y)))
 
-    epochs = 400
-    batch_size = 2048
+    epochs = 10
 
     log_dir = "/tmp/tensorboard/"
 
-    model.fit(x=X, y=Y,
-              batch_size=batch_size, epochs=epochs, verbose=1,
-              validation_split=0.2, shuffle=True, class_weight=class_weights,
+    model.fit(x=train_data_gen,
+              validation_data=test_data_gen,
+              class_weight=class_weights,
+              epochs=epochs,
+              verbose=1,
               callbacks=[callbacks.EarlyStopping(monitor="loss", min_delta=.0005, patience=3),
-                         callbacks.ModelCheckpoint(model_output, monitor='val_loss', verbose=1,
+                         callbacks.ModelCheckpoint("/tmp/checkpoint/", monitor='val_loss', verbose=1,
                                                    save_best_only=True, save_weights_only=False),
                          callbacks.TensorBoard(log_dir=log_dir, write_graph=False)])
 
